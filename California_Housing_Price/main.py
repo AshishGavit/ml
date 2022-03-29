0922c674d3480f8f934060a39f3988a61c5fc243
AshishGavit <ashishgavit2009@gmail.com>
2022-03-29 09:27:59 +0530

    Modified main file

--- California_Housing_Price/main.py
+++ California_Housing_Price/main.py
@@ -1,38 +1,42 @@
-from california_housing_price import *
+from data_preparation import *
 
 if __name__ == '__main__':
+    data_path = 'datasets\\housing\\'
+    img_path = 'images\\housing_img\\'
+    DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
+    HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
     loop = True
     while loop == True:
-        print('1 - Fetch & Extract Data \n2 - Read Data & Visualize \n3 - Add and Compare\n4 - Discove and Visualize \n5 - Data Preparation')
-
-        DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
-        HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+        print('1 - Fetch & Extract Data \n2 - Read Data & Visualize \n3 - Add and Compare\n4 - Discover And Visulize\n5 - Data Preparation\n')
         try:
             user_input = input('Enter number : ')
             user_input = int(user_input)
             if user_input == 1:
-                SDI = SaveDataAndImage(folder='housing', url=HOUSING_URL)
-                SDI.fetch_and_extract_data()
-            elif user_input == 2: 
+                from download_save_data import *
+                # create object of DownloadAndSave class
+                data = DownloadAndSave(url=HOUSING_URL)
+                data.fetch_and_extract_data(data_path)
+                data.save_fig(img_path, '')
+            elif user_input == 2:
+                from read_and_visulize import ReadData 
                 # create object of ReadData()
-                RD = ReadData(folder='housing')
-                RD.pandas_method()
-                RD.matplotlib_method()
+                rd = ReadData()
+                rd.pandas_method(data_path)
+                rd.matplotlib_method(data_path, img_path)
             elif user_input == 3:
+                from read_and_visulize import CompareProprtions
                 # create object of AddCompare()
-                AC = AddCompare(folder='housing')
-                AC.split_data()
-                AC.compare_proprtions()
+                cp = CompareProprtions()
+                cp.compare_proprtions(data_path)
             elif user_input == 4:
-                # create object of DiscoverAndVisulize
-                DAV = DiscoverAndVisulize(folder='housing')
-                DAV.visulaize_data()
-                DAV.correlation()
+                from read_and_visulize import DiscoverAndVisulize
+                da = DiscoverAndVisulize()
+                da.visulaize_data(data_path, img_path)
+                da.correlation(data_path, img_path)
             elif user_input == 5:
-                # create object of DataPreparation
-                DP = DataPreparation(folder='housing')
-                DP.null_value_with_imputer()
-                DP.convert_categorical_feature()
+                dp = DataPreparation()
+                dp.null_value(data_path)
         except ValueError:
             user_input = str(user_input)
             if user_input == 'exit':
